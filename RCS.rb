ead72242722015924404c930f3bf54f4f5ca57cc
Christian Encarnacion <encarc@rpi.edu>
2015-10-25 11:01:06 -0400

    Added some more functions

--- RCS.rb
+++ RCS.rb
@@ -1,21 +1,48 @@
 require 'pathname'
 require 'fileutils'
 
-def loadFiles(directory)
+def loadMusic(directory)
 	fileArray = []
-	FileUtils.cd(directory, :verbose => true)
+
 	Dir.foreach(directory) do |fileName|
 		if File.extname(fileName) == ".mp3"
 			fileArray.push(fileName)
 		end
-	end
-
+	end 
 	return fileArray
 end
 
+def selectTwoTracks(musicArray)
+	
+	if musicArray.length == 0
+		return []
+	end
 
-directory = "C:\\Users\\Christian\\Desktop\\LeagueMUSIC"
-print directory
+	Random.new_seed()
 
-array = loadFiles(directory)
-print array.count()
+	r1 = rand(musicArray.length())
+	r2 = rand(musicArray.length())
+	while r2 == r1
+		r2 = rand(musicArray.length())
+	end
+
+	twoTracks = [musicArray[r1], musicArray[r2]]
+	return twoTracks
+end
+	
+ 
+def redefineChampionSelect(originalDir, newDir, musicArray) 
+	if Dir.exist? originalDir and Dir.exist? newDir
+		FileUtils.cd(originalDir, :verbose => true)
+		FileUtils.cp(originalDir + File.basename(musicFile), newDir)
+		FileUtils.cd(newDir, :verbose => true)
+		if File.exist?("ChmpSlct_DraftMode.mp3")
+			File.delete("ChmpSlct_DraftMode.mp3")
+		end
+		File.rename((File.basename musicFile), "ChmpSlct_DraftMode.mp3")
+	end
+end
+
+	
+
+
