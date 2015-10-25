c0ab519d2e064296c42d8238ca66296cdfbcd448
Christian Encarnacion <encarc@rpi.edu>
2015-10-25 12:11:32 -0400

    Commented the batch file

--- League.bat
+++ League.bat
@@ -1,5 +1,10 @@
+
+::Make sure to change this to the path of your cloned version
 set rcsPATH="C:\Users\Christian\Desktop\Redefine-Champion-Select"
 cd /d %rcsPATH%
+
+::Runs ruby script
 ruby main.rb
 
+::Only change this if your game directory is different than this
 start "" "C:\Riot Games\League of Legends\lol.launcher.exe"
