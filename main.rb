66962ac0fc24827f07374d21d7f7281732cd9714
Christian Encarnacion <encarc@rpi.edu>
2015-10-25 12:09:49 -0400

    Commented main

--- main.rb
+++ main.rb
@@ -1,10 +1,13 @@
 load 'RCS.rb'
 
 if __FILE__ == $0
+	#changes this to your music directory
     musicDirectory = "C:\\Users\\Christian\\Desktop\\LeagueMUSIC\\"
+
+    #this can stay the same
     leagueDirectory = "C:\\Riot Games\\League of Legends\\RADS\\projects\\lol_air_client\\releases\\0.0.1.164\\deploy\\assets\\sounds\\ambient"
 
-
+    #Load the music files and move them
     musicArray = loadMusic(musicDirectory)
     redefineChampionSelect(musicDirectory, leagueDirectory, musicArray)
 end
